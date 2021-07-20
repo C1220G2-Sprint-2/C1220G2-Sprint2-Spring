93a8e8dfca21fbe762ea5c243ae7d3c62d79314e
sangle173 <76678575+sangle173@users.noreply.github.com>
2021-07-20 10:42:40 +0700

    sangld update v2

--- back_end_sprint_2/src/main/java/com/codegym/back_end_sprint_2/service/IReviewService.java
+++ back_end_sprint_2/src/main/java/com/codegym/back_end_sprint_2/service/IReviewService.java
@@ -1,7 +1,13 @@
 package com.codegym.back_end_sprint_2.service;
 
+import com.codegym.back_end_sprint_2.model.dto.ReviewDto;
+import com.codegym.back_end_sprint_2.model.entities.Notification;
 import com.codegym.back_end_sprint_2.model.entities.Review;
 
+import java.util.List;
+
 public interface IReviewService {
-    void save(Review review);
+    void save(ReviewDto reviewDto);
+    Notification findNotificationById(Long id);
+    List<Review> findAllReview();
 }
--- back_end_sprint_2/src/main/java/com/codegym/back_end_sprint_2/service/impl/ReviewServiceImpl.java
+++ back_end_sprint_2/src/main/java/com/codegym/back_end_sprint_2/service/impl/ReviewServiceImpl.java
@@ -1,18 +1,40 @@
 package com.codegym.back_end_sprint_2.service.impl;
 
+import com.codegym.back_end_sprint_2.model.dto.ReviewDto;
+import com.codegym.back_end_sprint_2.model.entities.Notification;
 import com.codegym.back_end_sprint_2.model.entities.Review;
+import com.codegym.back_end_sprint_2.repositories.INotificationRepository;
 import com.codegym.back_end_sprint_2.repositories.IReviewRepository;
+import com.codegym.back_end_sprint_2.repositories.ITeacherRepository;
 import com.codegym.back_end_sprint_2.service.IReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class ReviewServiceImpl implements IReviewService {
     @Autowired
     private IReviewRepository reviewRepository;
+    @Autowired
+    private ITeacherRepository teacherRepository;
+    @Autowired
+    private INotificationRepository notificationRepository;
 
     @Override
-    public void save(Review review) {
+    public void save(ReviewDto reviewDto) {
+        Review review = new Review(reviewDto.getTitle(), reviewDto.getContent(), reviewDto.getAttachFile(), reviewDto.isEnable(), reviewDto.getProgressReview(), teacherRepository.findByCode(reviewDto.getTeacherCode()), findNotificationById(reviewDto.getNotification()), LocalDateTime.now());
         reviewRepository.save(review);
+    }
+
+    @Override
+    public Notification findNotificationById(Long id) {
+        return notificationRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<Review> findAllReview() {
+        return reviewRepository.findAll();
     }
 }
