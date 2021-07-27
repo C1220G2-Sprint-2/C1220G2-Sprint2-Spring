dbe67fed0e592c56d6057bfd8f7d86beb62f9d89
tuannguyenquoc050397 <tuannguyenquoc15dt2@gmail.com>
2021-07-27 22:24:34 +0700

    27/07 10h24

--- back_end_sprint_2/src/main/java/com/codegym/back_end_sprint_2/repository/TeacherRepository.java
+++ back_end_sprint_2/src/main/java/com/codegym/back_end_sprint_2/repository/TeacherRepository.java
@@ -8,13 +8,13 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
 
-    @Query(value="select teacher.id, teacher.address, teacher.`code`, teacher.date_of_birth, teacher.email, teacher.`enable`, teacher.facebook, " +
-            "teacher.gender, teacher.image, teacher.`name`, teacher.phone, teacher.twitter, teacher.education_id, teacher.faculty_id " +
-            "from teacher " +
-            "inner join project on project.teacher_code = teacher.id " +
-            "inner join team on project.team_id = team.id " +
-            "where team.`name` = ?1 " +
-            "group by teacher.id ", nativeQuery = true)
+    @Query(value="select teacher.teacher_code, teacher.address , teacher.date_of_birth, teacher.email, teacher.`enable`, teacher.facebook, " +
+            "            teacher.gender, teacher.image, teacher.teacher_name, teacher.phone, teacher.twitter, teacher.education_id, teacher.faculty_id " +
+            "            from teacher " +
+            "            inner join project on project.teacher_code = teacher.teacher_code " +
+            "            inner join team on project.team_id = team.id " +
+            "            where team.`name` = ?1 " +
+            "            group by teacher.teacher_code;", nativeQuery = true)
     Teacher findTeacherByTeam(String team);
 
 }
